bb91fc5e694a8b0c837a60e963f6ab6fe1aaf825
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-01-05 21:44:33 +0200

    Create main page markup

---
+++ src/pages/layout/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppRouter} from "../../../router/AppRouter";
+
+
+function Main() {
+    return (
+        <main className="main">
+            <AppRouter/>
+        </main>
+    )
+}
+
+export default Main
